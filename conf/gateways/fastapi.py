72b1c7fc9e98f88d24667a9e23d9029fde7b1159
firminoneto11 <firminoneto11@hotmail.com>
2023-12-24 19:48:57 -0300

    chore: created gateways directory

---
+++ conf/gateways/fastapi.py
@@ -0,0 +1,25 @@
+from os import environ
+
+from django import setup
+from fastapi import FastAPI
+
+
+def get_fastapi_application():
+    from django.conf import settings
+
+    from conf.urls import routers
+
+    application = FastAPI(debug=settings.DEBUG, title=settings.FASTAPI_TITLE)
+
+    # Place middleware here
+
+    [application.include_router(router=router, prefix="/api") for router in routers]
+
+    return application
+
+
+environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+
+
+setup(set_prefix=False)
+app = get_fastapi_application()
