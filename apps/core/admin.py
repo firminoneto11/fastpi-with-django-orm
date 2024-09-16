bf8b1b97bd5c60ec61aa3818657bbccbea319b20
firminoneto11 <firminoneto11@hotmail.com>
2024-09-16 18:12:12 -0300

    chore: removed old code and added new deps

--- apps/core/admin.py
+++
@@ -1,1 +0,0 @@
-from django.contrib.admin import ModelAdmin
--- shared/exceptions.py
+++
@@ -1,8 +0,0 @@
-from fastapi.exceptions import HTTPException
-
-
-class EntityNotFoundError(HTTPException):
-    http_code = 404
-
-    def __init__(self, detail: str):
-        super().__init__(status_code=self.http_code, detail=detail, headers=None)
